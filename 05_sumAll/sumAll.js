f80fae968cab881461cba3c66ff34235c6a413d1
n0tinthemood <marvin.goertz.2908@gmail.com>
2021-12-12 00:05:05 +0100

    passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,23 +2,26 @@ function sumAll(...args) {
 
     //sorts numbers ascending
     args.sort(function(a, b){return a - b;});
-
+    
     let sum = 0;
 
+    //check, that passed parameters are type "number" only
+    if (typeof args[0] === "number" && typeof args[1] === "number") {
+
     //add first number until first number = second number, e.g 1 + 2 + 3 
-    for(; args[0] <= args[1]; args[0]++) {
-        if (args[0] < 0 || args[1] < 0) {
-            sum = "ERROR";
-            break;
-        }
-        else if (typeof args[0] !== "number" || typeof args[1] !== "number") {
-            sum = "ERROR"
-            break;
-        }
-        else {
-            sum += args[0];
-        }
+        for(; args[0] <= args[1]; args[0]++) {
+            if (args[0] < 0 || args[1] < 0) {
+                sum = "ERROR";
+                break;
+            }
+            else {
+                sum += args[0];
+            }
+        }    
     }
+    else {
+        sum = "ERROR";
+    } 
     return sum 
 }
 
