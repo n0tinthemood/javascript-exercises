c36256490a195255b765c95a23fbd8197176b17b
n0tinthemood <marvin.goertz.2908@gmail.com>
2021-12-11 21:41:38 +0100

    passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for(let i = 0; i <= args.length-1; i++) {
+        //if values arent present in array -> indexOf return -1, check for present values
+        let checkPresent =array.indexOf(args[i]);
+        if(checkPresent !== -1) 
+            //get index of to be removed arg and remove from array
+            array.splice(array.indexOf(args[i]),1);
+    }
+    return array;     
 };
 
 // Do not edit below this line
