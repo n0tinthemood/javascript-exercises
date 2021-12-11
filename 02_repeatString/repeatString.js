0a2c5b18181d2d7907e945fd4566b6e3d55b103c
n0tinthemood <marvin.goertz.2908@gmail.com>
2021-12-11 19:59:01 +0100

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,14 @@
 const repeatString = function(string, num) {
-    let resultString= "";
-    for (let i = 0; i < num; i++) {
-        resultString += string;
+    let resultString = '';
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            resultString += string;
+        }
+        return resultString;
     }
-    return resultString;
+    else {
+        return "ERROR";
+    }  
 };
 
 // Do not edit below this line
