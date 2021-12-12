2eb3c8d329b481732e06727af57ab7bd12980af5
n0tinthemood <marvin.goertz.2908@gmail.com>
2021-12-12 19:24:48 +0100

    passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear;
 
+    if(year % 2 === 0) {
+        isLeapYear = true;
+        if(year % 2 === 0 && year % 400 === 0) {
+            isLeapYear = true;
+        } else if(year % 2 === 0 && year % 100 === 0) {
+        isLeapYear = false;
+        }
+    } else {
+        isLeapYear = false;
+    }
+    return isLeapYear;
 };
 
 // Do not edit below this line
