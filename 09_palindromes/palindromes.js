31127ecd2a2bd8852c17c72a727b3910416054d2
n0tinthemood <marvin.goertz.2908@gmail.com>
2021-12-27 19:25:22 +0100

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let re = /[\W]/g
+    let lowerRegStr = str.toLowerCase().replace(re, "");
+    let reverseStr = lowerRegStr.split("").reverse().join("");
+    return reverseStr === lowerRegStr;
 };
 
 // Do not edit below this line
