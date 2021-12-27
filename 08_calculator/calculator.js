a184756d53f79108c0052ff2ddb7041cbc42039f
n0tinthemood <marvin.goertz.2908@gmail.com>
2021-12-27 19:12:33 +0100

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -13,16 +13,26 @@ const sum = function(sumUs) {
   return parseInt(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(multUs) {
+  const result = multUs.reduce((total, num) => {
+    return total * num;
+  })
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 0) {
+    return -1;
+  } else if(num === 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+
 };
 
 // Do not edit below this line
