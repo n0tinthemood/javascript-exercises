36462f6d1e92bce28e4c8f3debbc08a2f789104e
n0tinthemood <marvin.goertz.2908@gmail.com>
2021-12-27 19:46:17 +0100

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let result = [];
+    result[0] = 0;
+    result[1] = 1;
+    for(let i = 2; i <= num; i++) {
+        result[i] = result[i - 1] + result [i - 2];
+    }
+    if(num > 0) {
+        return result[num];
+    } else {
+        return "OOPS"
+    }
+    
 };
 
 // Do not edit below this line
